4d7b2268ba4099479f57b80120eced0720af7cf5
Max Joseph <maxwellbjoseph@gmail.com>
2020-05-24 09:49:10 -0600

    Attempting to use rstantools to avoid model recompilation

---
+++ R/mrmr-package.R
@@ -0,0 +1,16 @@
+#' The 'mrmr' package.
+#'
+#' @description CMR models in R.
+#'
+#' @docType package
+#' @name mrmr-package
+#' @aliases mrmr
+#' @useDynLib mrmr, .registration = TRUE
+#' @import methods
+#' @import Rcpp
+#' @importFrom rstan sampling
+#'
+#' @references
+#' Stan Development Team (2020). RStan: the R interface to Stan. R package version 2.19.3. https://mc-stan.org
+#'
+NULL
